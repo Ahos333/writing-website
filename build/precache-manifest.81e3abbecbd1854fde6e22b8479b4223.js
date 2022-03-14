ffd3cc02e3c25171ce9e79a3d8c32fe74d135000
Angelica Hoskins <anghos080@gmail.com>
2022-03-14 17:44:34 -0500

    update

---
+++ build/precache-manifest.81e3abbecbd1854fde6e22b8479b4223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56fbace06dbdb6166d3136a529100f8",
+    "url": "/github.io/writing-website/index.html"
+  },
+  {
+    "revision": "c103dff8bcc0ca1c8113",
+    "url": "/github.io/writing-website/static/css/main.ec4f771a.chunk.css"
+  },
+  {
+    "revision": "6fb85e5c674cbe258526",
+    "url": "/github.io/writing-website/static/js/2.401fb870.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/github.io/writing-website/static/js/2.401fb870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c103dff8bcc0ca1c8113",
+    "url": "/github.io/writing-website/static/js/main.058c59f5.chunk.js"
+  },
+  {
+    "revision": "71fd221a445343846bc7",
+    "url": "/github.io/writing-website/static/js/runtime-main.73be502f.js"
+  }
+]);
